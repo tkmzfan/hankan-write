278567fa627169ee67346b74241ba7c765631be4
tkmzfan <evrimkilic007@gmail.com>
2025-08-16 15:54:00 +0200

    fixed paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './',
+  base: '/hankan-write/', // for GitHub Pages
   build: {
     outDir: 'dist',
     minify: false,
